20d608f60fd79184f5ea9b13267e299c05038e18
Mike Bild <mike.bild@googlemail.com>
2018-10-03 08:08:53 +0200

    fix: rename example apps to uppercase

--- examples/react-advanced/App.js
+++ examples/react-advanced/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import Home from './components/Home';
-import About from './components/About';
-import Page from './components/Page';
-import { matchRoutes } from 'react-router-config';
 
 export default async ({ isServer, isProduction, request, port }) => {
+  // Automatic code splitting using dynamic imports
   const data = await import('./data.json');
+  const { default: Home } = await import('./components/Home');
+  const { default: About } = await import('./components/About');
+  const { default: Page } = await import('./components/Page');
+
+  // Create dynamically routes based on external data
   const routes = data.map(itm => ({
     path: `/page/${itm.id}`,
     exact: true,
+    // Pass external data down
     component: props => <Page {...props} data={itm} />,
   }));
 
+  // Return a array of routes (react-router-config)
   return [
     {
       path: '/',
       exact: true,
+      // Pass external data down
       component: props => <Home {...props} pages={data} />,
     },
     { path: '/about', exact: true, component: About },
